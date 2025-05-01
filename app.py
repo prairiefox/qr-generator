a9ed53da1c7facfb93a31e35f3c06c865f4cbf3e
prairiefox <tmbagrifood@gmail.com>
2025-05-01 11:50:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
-import segno
-from flask import Flask, request, Response
-
-app = Flask(__name__)
-
 @app.route("/qr")
 def generate_qr():
+    import io
     data = request.args.get("data", "")
     fmt = request.args.get("format", "svg").lower()
+    qr = segno.make(data, error='l')
 
-    qr = segno.make(data, error='l')  # ECC = L
+    buffer = io.BytesIO()
 
     if fmt == "png":
-        out = qr.png_as_base64_str(scale=5, border=0)
-        return Response(
-            response=bytes.fromhex(out),
-            content_type="image/png"
-        )
-    else:  # default to SVG
-        out = qr.to_svg(None, xmldecl=False, border=0)
-        return Response(out, mimetype="image/svg+xml")
+        qr.save(buffer, kind='png', scale=5, border=0)
+        buffer.seek(0)
+        return Response(buffer.read(), mimetype='image/png')
+    else:
+        qr.save(buffer, kind='svg', xmldecl=False, border=0)
+        buffer.seek(0)
+        return Response(buffer.read(), mimetype='image/svg+xml')
