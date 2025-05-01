85a80bb60c24cd0f685b420defbd75e344f72b1d
prairiefox <tmbagrifood@gmail.com>
2025-05-01 11:42:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route("/qr")
 def generate_qr():
     data = request.args.get("data", "")
+    fmt = request.args.get("format", "svg").lower()
+
     qr = segno.make(data, error='l')  # ECC = L
-    out = qr.to_svg(None, xmldecl=False, border=0)  # Quiet zone = 0
-    return Response(out, mimetype='image/svg+xml')
+
+    if fmt == "png":
+        out = qr.png_as_base64_str(scale=5, border=0)
+        return Response(
+            response=bytes.fromhex(out),
+            content_type="image/png"
+        )
+    else:  # default to SVG
+        out = qr.to_svg(None, xmldecl=False, border=0)
+        return Response(out, mimetype="image/svg+xml")
