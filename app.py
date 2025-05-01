6e60932180be92bb60bf5c38e96d2e659cc9e745
prairiefox <tmbagrifood@gmail.com>
2025-05-01 12:13:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def generate_qr():
     buffer = io.BytesIO()
 
     if fmt == "png":
-        qr.save(buffer, kind='png', scale=10, border=4)
+        qr.save(buffer, kind='png', scale=10, border=2)
         buffer.seek(0)
         return Response(buffer.read(), mimetype='image/png')
     else:
-        qr.save(buffer, kind='svg', xmldecl=False, scale=10, border=4)
+        qr.save(buffer, kind='svg', xmldecl=False, scale=10, border=2)
         buffer.seek(0)
         return Response(buffer.read(), mimetype='image/svg+xml')
